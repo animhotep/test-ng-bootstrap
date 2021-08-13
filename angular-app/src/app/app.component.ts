a3c8972c2bf78a9ae747def3aca26fe8cacb2805
Your Name <you@example.com>
2021-08-13 18:05:35 +0300

    go to material

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
 
 interface Driver {
   driverName: string,
@@ -20,20 +22,31 @@ interface Driver {
   styleUrls: ['./app.component.css']
 })
 
-
 export class AppComponent implements OnInit {
   drivers!: Driver[];
+  displayedColumns!: string[];
+  dataSource!: MatTableDataSource<Driver>;
+  @ViewChild(MatSort) sort!: MatSort;
 
   ngOnInit(): void {
    fetch('http://localhost:4000/')
      .then(r => r.json())
-     .then((drivers:Driver[]) => this.drivers = drivers.map(d => {
-       console.log(d.location[0])
-       d.lat = d.location[0];
-       d.lon = d.location[1];
-       return d;
-     }));
+     .then((drivers:Driver[]) => {
+       this.drivers = drivers.map(d => {
+         d.lat = d.location[0];
+         d.lon = d.location[1];
+         return d;
+       });
+       this.displayedColumns =  ['driverName', 'driverCityOrigin', 'driverPhone', 'driverGender', 'driverLanguage', 'location'];
+       this.dataSource = new MatTableDataSource(this.drivers);
+       this.dataSource.sort = this.sort;
+     });
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+  }
 
 }
